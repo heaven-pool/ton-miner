2104a145c2bd59fcbf6f111b44992e112bde9d4b
qwedsazxc78 <qwedsazxc78@gmail.com>
2022-01-24 21:23:37 +0800

    add

---
+++ env-hive-os.sh
@@ -0,0 +1,10 @@
+# dev
+apt install -y python3-pip git
+
+# project
+git clone https://github.com/heaven-pool/ton-miner
+
+
+pip3 install requests pyinstaller
+pip3 install "numpy<1.15"
+pip3 install "pyopencl<2018.3"
