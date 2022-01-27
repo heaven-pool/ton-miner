27a3604deb3ed75bd08c6536241955773801cfc7
Leo Liu <leo.liu@hp.com>
2022-01-27 22:51:06 +0800

    Download for all platforms

--- src/app/package.py
+++ src/app/package.py
@@ -4,62 +4,65 @@ import zipfile
 from pathlib import Path
 import os
 import glob
+import re
 import shutil
 import tarfile
-# from logguru import logger
+from loguru import logger
 
-BASE_URL = 'https://github.com/tontechio/pow-miner-gpu/releases/latest/download'
-WIN_MINER = [f'{BASE_URL}/minertools-cuda-windows-x86-64.zip',
-             f'{BASE_URL}/minertools-opencl-windows-x86-64.zip']
+VERSION = '20211230.1'
+BASE_URL = f'https://github.com/tontechio/pow-miner-gpu/releases/download/{VERSION}'
 
-UBUNTU_1804_MINER = [f'{BASE_URL}/minertools-cuda-ubuntu-18.04-x86-64.tar.gz',
-                     f'{BASE_URL}/minertools-opencl-ubuntu-18.04-x86-64.tar.gz']
-UBUNTU_2004_MINER = [f'{BASE_URL}/minertools-cuda-ubuntu-20.04-x86-64.tar.gz',
-                     f'{BASE_URL}/minertools-opencl-ubuntu-20.04-x86-64.tar.gz']
+HIVEOS_VERSION = 'h.20211230.1'
+HIVEOS_BASE_URL = f'https://github.com/tontechio/pow-miner-gpu-hiveos/releases/download/{HIVEOS_VERSION}'
 
-HIVE_BASE_URL = 'https://github.com/tontechio/pow-miner-gpu-hiveos/releases/download'
-HIVEOS_MINER = [f'{HIVE_BASE_URL}/h.20211230.1/tonminer_opencl_hiveos_x86_64-h.20211230.1.tar.gz',
-                f'{HIVE_BASE_URL}/h.20211230.1/tonminer_cuda_hiveos_x86_64-h.20211230.1.tar.gz']
+MINER_TOOLS = {
+    'windows': [
+        f'{BASE_URL}/minertools-cuda-windows-x86-64.zip',
+        f'{BASE_URL}/minertools-opencl-windows-x86-64.zip'
+    ],
+    'ubuntu18': [
+        f'{BASE_URL}/minertools-cuda-ubuntu-18.04-x86-64.tar.gz',
+        f'{BASE_URL}/minertools-opencl-ubuntu-18.04-x86-64.tar.gz'
+    ],
+    'ubuntu20': [
+        f'{BASE_URL}/minertools-cuda-ubuntu-20.04-x86-64.tar.gz',
+        f'{BASE_URL}/minertools-opencl-ubuntu-20.04-x86-64.tar.gz'
+    ],
+    'hiveos': [
+        f'{HIVEOS_BASE_URL}/tonminer_opencl_hiveos_x86_64-{HIVEOS_VERSION}.tar.gz',
+        f'{HIVEOS_BASE_URL}/tonminer_cuda_hiveos_x86_64-{HIVEOS_VERSION}.tar.gz'
+    ]
+}
 
-BUILD_PACK = ['window', 'ubuntu18', 'ubuntu20', 'hiveos']
-REMOVE_PATTENS = ['*.sh', 'tonlib*', '*.service', 'release.json',  '*.conf']
+IGNORE_PATTENS = ['*.sh', 'tonlib*', '*.service', 'release.json', '*.conf', '*.json', '*.md']
 
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-ROOT_PATH = Path(BASE_PATH).parents[1]
-# steps
-# 1. os
-# 2. download files
-# 3. unzip, overwrite file and rename
-# 4. put binary or build exec
+PROJECT_ROOT_PATH = Path(os.path.dirname(os.path.abspath(__file__))).parents[1]
 
 
-def os_type():
-    os_name = platform.system()
-    if os_name == 'Linux':
-        return 'Linux'
-    elif os_name == 'Windows':
-        return 'Windows'
-    elif os_name == 'Darwin':
-        return 'Not support mac'
+def get_os_type():
+    uname = platform.uname()
+    if uname.system == 'Windows':
+        return 'windows'
+    elif uname.system == 'Linux':
+        if uname.node == 'ubuntu':
+            version_id = re.search('VERSION_ID=(.*)\\n', open('/etc/os-release', 'r').read()).group(0)
+            if re.search('20', version_id):
+                return 'ubuntu20'
+            elif re.search('18', version_id):
+                return 'ubuntu18'
+            else:
+                NotImplementedError(uname, version_id)
+        elif 'hiveos' in uname.release:
+            return 'hiveos'
+        else:
+            NotImplementedError(uname)
     else:
-        return 'Not support OS'
-
-
-def power_miner_url(os_type: str):
-    if os_type == 'window':
-        return WIN_MINER
-    elif os_type == 'ubuntu18':
-        return UBUNTU_1804_MINER
-    elif os_type == 'ubuntu20':
-        return UBUNTU_2004_MINER
-    elif os_type == 'hiveos':
-        return HIVEOS_MINER
-    else:
-        return null
+        NotImplementedError(uname)
 
 
 def download_url(url, dst_folder, chunk_size=128):
     filename = Path(dst_folder, url.split('/')[-1])
+    logger.info(f'Download {url}')
     r = requests.get(url)
 
     with open(filename, 'wb') as fd:
@@ -67,21 +70,22 @@ def download_url(url, dst_folder, chunk_size=128):
             fd.write(chunk)
 
 
-def download_folder(os_type: str):
-    folder = Path(ROOT_PATH, 'build', os_type)
+def create_download_folder(os_type: str):
+    folder = Path(PROJECT_ROOT_PATH, 'build', os_type)
     os.makedirs(folder, exist_ok=True)
-    print(f"create {folder}")
+    logger.info(f'Create {folder}')
     return folder
 
 
-def bin_folder(os_type: str):
-    folder = Path(ROOT_PATH, 'bin', os_type)
+def create_bin_folder(os_type: str):
+    folder = Path(PROJECT_ROOT_PATH, 'bin', os_type)
     os.makedirs(folder, exist_ok=True)
-    print(f"create {folder}")
+    logger.info(f'Create {folder}')
     return folder
 
 
-def unzip_each_file(src_file: str, dst_folder: str):
+def unzip_file(src_file: str, dst_folder: str):
+    logger.info(f'Unzip {src_file}')
     with zipfile.ZipFile(src_file, 'r') as zf:
         for member in zf.namelist():
             filename = os.path.basename(member)
@@ -89,12 +93,13 @@ def unzip_each_file(src_file: str, dst_folder: str):
                 continue
 
             source = zf.open(member)
-            target = open(os.path.join(dst_folder, filename), "wb")
+            target = open(os.path.join(dst_folder, filename), 'wb')
             with source, target:
                 shutil.copyfileobj(source, target)
 
 
-def untar_each_file(src_file: str, dst_folder: str):
+def untar_file(src_file: str, dst_folder: str):
+    logger.info(f'Untar {src_file}')
     with tarfile.open(src_file, 'r') as tf:
         for member in tf.getmembers():
             if member.isreg():  # skip if the TarInfo is not files
@@ -103,44 +108,39 @@ def untar_each_file(src_file: str, dst_folder: str):
                 tf.extract(member, dst_folder)  # extract
 
 
-def unzip_folder(src_folder: str, dst_folder: str):
-    files = glob.glob(f"{src_folder}/*.zip") + \
-        glob.glob(f"{src_folder}/*.tar.gz")
+def unarchive_each_files(src_folder: str, dst_folder: str):
+    logger.info(f'Unarchive')
+    files = glob.glob(f'{src_folder}/*.zip') + \
+        glob.glob(f'{src_folder}/*.tar.gz')
 
     for file in files:
         if file.split('.')[-1] == 'zip':
-            unzip_each_file(file, dst_folder)
+            unzip_file(file, dst_folder)
         elif file.split('.')[-1] == 'gz':
-            untar_each_file(file, dst_folder)
+            untar_file(file, dst_folder)
         else:
-            print('file type error')
+            logger.info('file type error')
 
 
 def remove_patten_files(dst_folder: str, pattern: str = 'tonlib*'):
-    files = glob.glob(f"{dst_folder}/{pattern}")
+    files = glob.glob(f'{dst_folder}/{pattern}')
     for file in files:
+        logger.info(f'Remove {file}')
         os.remove(file)
 
 
-def downloader(os_type: str):
-    if os_type in BUILD_PACK:
-        folder = download_folder(os_type)
-        urls = power_miner_url(os_type)
-        for url in urls:
-            download_url(url, folder)
-    else:
-        print('os_type error')
+def download(os_type: str):
+    folder = create_download_folder(os_type)
+    for url in MINER_TOOLS[os_type]:
+        logger.info(f'Download {url}')
+        download_url(url, folder)
 
 
-# os_type = 'ubuntu20'
-# os_type = 'window'
-
-
-for os_type in BUILD_PACK:
-    src_folder = download_folder(os_type)
-    dst_folder = bin_folder(os_type)
-    downloader(os_type)
-    unzip_folder(src_folder, dst_folder)
-
-    [remove_patten_files(dst_folder, remove_patten)
-     for remove_patten in REMOVE_PATTENS]
+if __name__ == "__main__":
+    for os_type in MINER_TOOLS:
+        src_folder = create_download_folder(os_type)
+        dst_folder = create_bin_folder(os_type)
+        download(os_type)
+        unarchive_each_files(src_folder, dst_folder)
+        for remove_patten in IGNORE_PATTENS:
+            remove_patten_files(dst_folder, remove_patten)
